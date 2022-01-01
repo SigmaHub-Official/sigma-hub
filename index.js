91cf20549d5d0da436ad89a395ff0f3e3ae15c2f
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2022-01-01 20:33:31 +0530

    make it better

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const serverless = require('serverless-http');
+const router = express.Router();
+
+console.log("Index.js is running");
+
+// Static files
+app.use('/static', express.static('public'));
+app.use(express.json());
+
 
 // Front display setup
 app.set('view engine', 'pug');
-app.set('views','./views');
+app.set('views','./public/views');
+
+
 
 // Load home page
 app.get('/', function(req, res){
-    res.render("index")
+    res.render("index");
 });
+
+app.listen(3030);
